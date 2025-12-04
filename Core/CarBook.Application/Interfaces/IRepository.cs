e7628856c921d299ed87e3515673f08737b1e87a
elifkoca2 <koca24988@gmail.com>
2025-12-04 22:32:52 +0300

    Yeni Eklemeler Yapıldı.

---
+++ Core/CarBook.Application/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.İnterfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task CreateAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task Remove (T entity);
+    }
+}
